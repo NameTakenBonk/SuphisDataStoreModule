70b8451c9ba4199781ba5b8dbae242268b2f2165
Alternative <83465599+NameTakenBonk@users.noreply.github.com>
2022-11-11 14:29:40 +0000

    V0.3

--- DataStore.lua
+++ DataStore.lua
@@ -1,16 +1,33 @@
--- Version: 0.1 (BETA)
+-- Version: 0.3 (BETA)
 local module = {}
 module.__index = module
 
 local dataStoreService = game:GetService("DataStoreService")
 local memoryStoreService = game:GetService("MemoryStoreService")
-local states = {["Destroyed"] = false, ["Loading"] = false, ["Closed"] = false, ["Closing"] = false, ["Opening"] = false, ["Open"] = true, ["Saving"] = true}
+local activeStates = {["Destroyed"] = false, ["Loading"] = false, ["Closed"] = false, ["Closing"] = false, ["Opening"] = false, ["Open"] = true, ["Saving"] = true}
 local dataStores = {}
+
+local function Clone(original)
+	if type(original) ~= "table" then return original end
+	local clone = {}
+	for key, value in original do clone[key] = Clone(value) end
+	return clone
+end
+
+local function Reconcile(target, template)	
+	for key, value in template do
+		if target[key] == nil then
+			target[key] = Clone(value)
+		elseif type(target[key]) == "table" and type(value) == "table" then
+			Reconcile(target[key], value)
+		end
+	end
+end
 
 local function State(self, state)
 	self.State = state
-	if self.Active ~= states[state] then
-		self.Active = states[state]
+	if self.Active ~= activeStates[state] then
+		self.Active = activeStates[state]
 		self.StateBindableEvent:Fire(state)
 		self.ActiveBindableEvent:Fire(self.Active)
 	else
@@ -39,33 +56,44 @@ local function StopTimer(self)
 end
 
 local function Lock(self)
-	local success, value = pcall(self.MemoryStore.UpdateAsync, self.MemoryStore, "JobId", function(value) return if value == nil or value == game.JobId then game.JobId else nil end, self.Interval + 30)
-	if success == false then return "MemoryStore" end
-	if value == nil then return "Locked" end
+	local jobId = nil
+	local success, value = pcall(self.MemoryStore.UpdateAsync, self.MemoryStore, "JobId", function(value) jobId = value return if jobId == nil or jobId == game.JobId then game.JobId else nil end, self.Interval + 30)
+	if success == false then return "MemoryStore", value end
+	if value == nil then return "Locked", jobId end
 end
 
 local function Unlock(self)
-	local success, value = pcall(self.MemoryStore.UpdateAsync, self.MemoryStore, "JobId", function(value) return if value == game.JobId then game.JobId else nil end, 0)
-	if success == false then return "MemoryStore" end
-	if value == nil then return "Locked" end
+	local jobId = nil
+	local success, value = pcall(self.MemoryStore.UpdateAsync, self.MemoryStore, "JobId", function(value) jobId = value return if jobId == game.JobId then game.JobId else nil end, 0)
+	if success == false then return "MemoryStore", value end
+	if value == nil and jobId ~= nil then return "Locked", jobId end
 end
 
-local function Load(self, default)
+local function Load(self)
 	local success, value, info = pcall(self.DataStore.GetAsync, self.DataStore, self.Key)
-	if success == false then return "DataStore" end
-	self.Value = value or default
-	self.CreatedTime = info.CreatedTime
-	self.UpdatedTime = info.UpdatedTime
-	self.Version = info.Version
-	self.Metadata = info:GetMetadata()
-	self.UserIds = info:GetUserIds()
+	if success == false then return "DataStore", value end
+	self.Value = value
+	if info == nil then
+		self.CreatedTime, self.UpdatedTime, self.Version, self.UserIds, self.Metadata = 0, 0, "", {}, {}
+	else
+		self.CreatedTime, self.UpdatedTime, self.Version, self.UserIds, self.Metadata = info.CreatedTime, info.UpdatedTime, info.Version, info:GetUserIds(), info:GetMetadata()
+	end
 end
 
 local function Save(self)
 	self.SavingBindableEvent:Fire()
-	self.Options:SetMetadata(self.Metadata)
-	local success = pcall(self.DataStore.SetAsync, self.DataStore, self.Key, self.Value, self.UserIds, self.Options)
-	if success == false then return "DataStore" end
+	if type(self.value) ~= "table" and self.Value == self.PreviousValue then return end
+	self.PreviousValue = self.Value
+	if self.Value == nil then
+		local success, value, info = pcall(self.DataStore.RemoveAsync, self.DataStore, self.Key)
+		if success == false then return "DataStore", value end
+		self.CreatedTime, self.UpdatedTime, self.Version, self.UserIds, self.Metadata = 0, 0, "", {}, {}
+	else
+		self.Options:SetMetadata(self.Metadata)
+		local success, value = pcall(self.DataStore.SetAsync, self.DataStore, self.Key, self.Value, self.UserIds, self.Options)
+		if success == false then return "DataStore", value end
+		self.Version = value
+	end
 	self.SavedTime = os.clock()
 end
 
@@ -82,7 +110,7 @@ module.new = function(name, scope, key)
 	self.UserIds = {}
 	self.Metadata = {}
 	self.Id = id
-	self.Key = tostring(key)
+	self.Key = key
 	self.State = "Closed"
 	self.Active = false
 	self.CreatedTime = 0
@@ -111,33 +139,35 @@ module.find = function(name, scope, key)
 	return dataStores[id]
 end
 
-module.Open = function(self, default)
+module.Open = function(self, template)
 	if self.State == "Closed" then
 		State(self, "Opening")
-		local memoryStoreError = Lock(self)
-		if memoryStoreError ~= nil then State(self, "Closed") return memoryStoreError end
+		local errorType, errorMessage = Lock(self)
+		if errorType ~= nil then State(self, "Closed") return errorType, errorMessage end
 		StartTimer(self)
-		local dataStoreError = Load(self, default)
-		if dataStoreError ~= nil then StopTimer(self) Unlock(self) State(self, "Closed") return dataStoreError end
+		local errorType, errorMessage = Load(self)
+		if errorType ~= nil then StopTimer(self) Unlock(self) State(self, "Closed") return errorType, errorMessage end
+		self:Reconcile(template)
 		State(self, "Open")
 		return
 	end
 	if self.State == "Open" or self.State == "Saving" then return end
-	if self.State == "Destroyed" then return "State" end
+	if self.State == "Destroyed" then return "State", self.State end
 	self.PrivateEvent:Wait()
-	return self:Open(default)
+	return self:Open(template)
 end
 
-module.Load = function(self, default)
+module.Load = function(self, template)
 	if self.State == "Closed" then
 		State(self, "Loading")
-		local dataStoreError = Load(self, default)
+		local errorType, errorMessage = Load(self)
+		self:Reconcile(template)
 		State(self, "Closed")
-		return dataStoreError
+		return errorType, errorMessage
 	end
-	if self.State == "Destroyed" or self.State == "Opening" or self.State == "Open" or self.State == "Saving" then return "State" end
+	if self.State == "Destroyed" or self.State == "Opening" or self.State == "Open" or self.State == "Saving" then return "State", self.State end
 	self.PrivateEvent:Wait()
-	return self:Load(default)
+	return self:Load(template)
 end
 
 module.Save = function(self)
@@ -146,19 +176,19 @@ module.Save = function(self)
 		Throttle(self)
 		if Lock(self) == nil then
 			StartTimer(self)
-			local dataStoreError = Save(self)
+			local errorType, errorMessage = Save(self)
 			State(self, "Open")
-			return dataStoreError
+			return errorType, errorMessage
 		else
 			State(self, "Closing")
 			StopTimer(self)
-			local dataStoreError = Save(self)
+			local errorType, errorMessage = Save(self)
 			Unlock(self)
 			State(self, "Closed")
-			return dataStoreError
+			return errorType, errorMessage
 		end
 	end
-	if self.State == "Destroyed" or self.State == "Loading" or self.State == "Closed" or self.State == "Closing" then return "State" end
+	if self.State == "Destroyed" or self.State == "Loading" or self.State == "Closed" or self.State == "Closing" then return "State", self.State end
 	self.PrivateEvent:Wait()
 	return self:Save()
 end
@@ -176,7 +206,7 @@ module.Close = function(self, save)
 		return
 	end
 	if self.State == "Closed" then return end
-	if self.State == "Destroyed" then return "State" end
+	if self.State == "Destroyed" then return "State", self.State end
 	self.PrivateEvent:Wait()
 	return self:Close(save)
 end
@@ -195,6 +225,18 @@ module.Destroy = function(self, save)
 	if self.State == "Destroyed" then return end
 	self.PrivateEvent:Wait()
 	return self:Destroy(save)
+end
+
+module.Clone = function(self)
+	return Clone(self.Value)
+end
+
+module.Reconcile = function(self, template)	
+	if self.Value == nil then
+		self.Value = Clone(template)
+	elseif type(self.Value) == "table" and type(template) == "table" then
+		Reconcile(self.Value, template)
+	end
 end
 
 return module
