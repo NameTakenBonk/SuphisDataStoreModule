074a80c32f02343d42e10acc35e832d0da49b16c
Alternative <83465599+NameTakenBonk@users.noreply.github.com>
2022-11-30 17:34:40 +0000

    V0.5

--- DataStore.lua
+++ DataStore.lua
@@ -1,19 +1,29 @@
--- Version: 0.4 (BETA)
+-- Version: 0.6 (BETA)
 
 local OpenTask, LoadTask, SaveTask, CloseTask, DestroyTask, AddTask, DoTasks
-local Lock, Unlock, Load, Save, StartSaveTimer, StopSaveTimer, SaveTimerEnded, StartLockTimer, StopLockTimer, LockTimerEnded, Clone, Reconcile
+local Lock, Unlock, Load, Save, StartSaveTimer, StopSaveTimer, SaveTimerEnded, StartLockTimer, StopLockTimer, LockTimerEnded, Clone, Reconcile, Compress, Decompress, Encode, Decode
 local signalModule = require(11670710927)
 local dataStoreService = game:GetService("DataStoreService")
 local memoryStoreService = game:GetService("MemoryStoreService")
+local httpService = game:GetService("HttpService")
 local privateIndex, dataStoreMetatable = {}, {}
 local constructor, property, dataStore = {}, {}, {}
 local dataStores = {}
 local permissions = {
 	["Value"] = true, ["Metadata"] = true, ["UserIds"] = true, ["SaveInterval"] = true, ["LockInterval"] = true, ["SaveBeforeClose"] = true,
-	["Id"] = false, ["Key"] = false, ["State"] = false, ["CreatedTime"] = false, ["UpdatedTime"] = false, ["Version"] = false,
+	["Id"] = false, ["Key"] = false, ["State"] = false, ["CreatedTime"] = false, ["UpdatedTime"] = false, ["Version"] = false, ["CompressedValue"] = false,
 	["StateChanged"] = false, ["Saving"] = false,
-	["Open"] = false, ["Load"] = false, ["Save"] = false, ["Close"] = false, ["Destroy"] = false, ["Clone"] = false, ["Reconcile"] = false,
+	["Open"] = false, ["Load"] = false, ["Save"] = false, ["Close"] = false, ["Destroy"] = false, ["Clone"] = false, ["Reconcile"] = false, ["Usage"] = false,
 }
+local characters = {
+	[0] = "0","1","2","3","4","5","6","7","8","9",
+	"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+	"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+	"!","$","%","&","'",",",".","/",":",";","=","?","@","[","]","^","_","`","{","}","~"
+}
+local bytes = {[string.byte(characters[0])] = 0}
+for i, character in ipairs(characters) do bytes[string.byte(character)] = i end
+local base = #characters + 1
 
 
 -- Types
@@ -28,13 +38,14 @@ export type DataStore = {
 	UserIds: {[number]: number},
 	SaveInterval: number,
 	LockInterval: number,
-	SaveBeforeClose: boolean,
+	SaveBeforeClose: number,
 	Id: string,
 	Key: string,
 	State: boolean?,
 	CreatedTime: number,
 	UpdatedTime: number,
 	Version: string,
+	CompressedValue: string,
 	StateChanged: signalModule.Signal,
 	Saving: signalModule.Signal,
 	Open: (self: DataStore, template: any?) -> (string?, string?),
@@ -44,6 +55,7 @@ export type DataStore = {
 	Destroy: (self: DataStore) -> (string?, string?),
 	Clone: (self: DataStore) -> any,
 	Reconcile: (self: DataStore, template: any) -> (),
+	Usage: (self: DataStore) -> (number, number),
 }
 
 
@@ -72,13 +84,14 @@ constructor.new = function(name, scope, key)
 	dataStoreObject.UserIds = {}
 	dataStoreObject.SaveInterval = 30
 	dataStoreObject.LockInterval = 30
-	dataStoreObject.SaveBeforeClose = true
+	dataStoreObject.SaveBeforeClose = 2
 	dataStoreObject.Id = id
 	dataStoreObject.Key = key
 	dataStoreObject.State = false
 	dataStoreObject.CreatedTime = 0
 	dataStoreObject.UpdatedTime = 0
 	dataStoreObject.Version = ""
+	dataStoreObject.CompressedValue = ""
 	dataStoreObject.StateChanged = signalModule.new()
 	dataStoreObject.Saving = signalModule.new()
 	dataStoreObject.Tasks = {}
@@ -112,7 +125,7 @@ end
 
 property.SaveInterval = function(proxy, value)
 	if type(value) ~= "number" then error("Attempt to set SaveInterval failed: Passed value is not a number", 3) end
-	if value < 10 and value ~= 0 then error("Attempt to set SaveInterval failed: Passed value is less then 10 and not 0", 3) end
+	if value < 6 and value ~= 0 then error("Attempt to set SaveInterval failed: Passed value is less then 6 and not 0", 3) end
 	local dataStoreObject = proxy[privateIndex]
 	if value == 0 then StopSaveTimer(dataStoreObject) elseif dataStoreObject.SaveInterval == 0 then StartSaveTimer(dataStoreObject) end
 	dataStoreObject.SaveInterval = value
@@ -120,13 +133,14 @@ end
 
 property.LockInterval = function(proxy, value)
 	if type(value) ~= "number" then error("Attempt to set LockInterval failed: Passed value is not a number", 3) end
-	if value < 10 then error("Attempt to set LockInterval failed: Passed value is less then 10", 3) end
+	if value < 6 then error("Attempt to set LockInterval failed: Passed value is less then 6", 3) end
 	if value > 3887970 then error("Attempt to set LockInterval failed: Passed value is more then 3887970", 3) end
 	proxy[privateIndex].LockInterval = value
 end
 
 property.SaveBeforeClose = function(proxy, value)
-	if type(value) ~= "boolean" then error("Attempt to set SaveBeforeClose failed: Passed value is not a boolean", 3) end
+	if type(value) ~= "number" then error("Attempt to set SaveBeforeClose failed: Passed value is not a number", 3) end
+	if value < 0 then error("Attempt to set SaveBeforeClose failed: Passed value is less then 0", 3) end
 	proxy[privateIndex].SaveBeforeClose = value
 end
 
@@ -162,6 +176,21 @@ dataStore.Reconcile = function(proxy, template)
 		dataStoreObject.Value = Clone(template)
 	elseif type(dataStoreObject.Value) == "table" and type(template) == "table" then
 		Reconcile(dataStoreObject.Value, template)
+	end
+end
+
+dataStore.Usage = function(proxy)
+	local dataStoreObject = proxy[privateIndex]
+	if type(dataStoreObject.Metadata.Compress) ~= "table" then
+		local characters = #httpService:JSONEncode(dataStoreObject.Value)
+		return characters, characters / 4194303
+	else
+		local level = dataStoreObject.Metadata.Compress.Level or 2
+		local decimals = 10 ^ (dataStoreObject.Metadata.Compress.Decimals or 3)
+		local safety = if dataStoreObject.Metadata.Compress.Safety == nil then true else dataStoreObject.Metadata.Compress.Safety
+		dataStoreObject.CompressedValue = Compress(dataStoreObject.Value, level, decimals, safety)
+		local characters = #httpService:JSONEncode(dataStoreObject.CompressedValue)
+		return characters, characters / 4194303
 	end
 end
 
@@ -215,7 +244,7 @@ CloseTask = function(dataStoreObject, parameters)
 	dataStoreObject.State = false
 	StopLockTimer(dataStoreObject)
 	StopSaveTimer(dataStoreObject)
-	if dataStoreObject.SaveBeforeClose == true then Save(dataStoreObject) end
+	for i = 1, dataStoreObject.SaveBeforeClose do if Save(dataStoreObject) == nil then break end end
 	Unlock(dataStoreObject)
 	dataStoreObject.StateChanged:Fire(false)
 end
@@ -226,7 +255,7 @@ DestroyTask = function(dataStoreObject, parameters)
 		dataStoreObject.State = nil
 		StopLockTimer(dataStoreObject)
 		StopSaveTimer(dataStoreObject)
-		if dataStoreObject.SaveBeforeClose == true then Save(dataStoreObject) end
+		for i = 1, dataStoreObject.SaveBeforeClose do if Save(dataStoreObject) == nil then break end end
 		Unlock(dataStoreObject)
 	end
 	dataStoreObject.State = nil
@@ -275,11 +304,17 @@ end
 Load = function(dataStoreObject)
 	local success, value, info = pcall(dataStoreObject.DataStore.GetAsync, dataStoreObject.DataStore, dataStoreObject.Key)
 	if success == false then warn("DataStore(" .. dataStoreObject.Id .. "):", value) return "DataStore", value end
-	dataStoreObject.Value = value
 	if info == nil then
 		dataStoreObject.Metadata, dataStoreObject.UserIds, dataStoreObject.CreatedTime, dataStoreObject.UpdatedTime, dataStoreObject.Version = {}, {}, 0, 0, ""
 	else
 		dataStoreObject.Metadata, dataStoreObject.UserIds, dataStoreObject.CreatedTime, dataStoreObject.UpdatedTime, dataStoreObject.Version = info:GetMetadata(), info:GetUserIds(), info.CreatedTime, info.UpdatedTime, info.Version
+	end
+	if type(dataStoreObject.Metadata.Compress) ~= "table" then
+		dataStoreObject.Value = value
+	else
+		dataStoreObject.CompressedValue = value
+		local decimals = 10 ^ (dataStoreObject.Metadata.Compress.Decimals or 3)
+		dataStoreObject.Value = Decompress(dataStoreObject.CompressedValue, decimals)
 	end
 end
 
@@ -289,9 +324,18 @@ Save = function(dataStoreObject)
 		local success, value, info = pcall(dataStoreObject.DataStore.RemoveAsync, dataStoreObject.DataStore, dataStoreObject.Key)
 		if success == false then warn("DataStore(" .. dataStoreObject.Id .. "):", value) return "DataStore", value end
 		dataStoreObject.Metadata, dataStoreObject.UserIds, dataStoreObject.CreatedTime, dataStoreObject.UpdatedTime, dataStoreObject.Version = {}, {}, 0, 0, ""
-	else
+	elseif type(dataStoreObject.Metadata.Compress) ~= "table" then
 		dataStoreObject.Options:SetMetadata(dataStoreObject.Metadata)
 		local success, value = pcall(dataStoreObject.DataStore.SetAsync, dataStoreObject.DataStore, dataStoreObject.Key, dataStoreObject.Value, dataStoreObject.UserIds, dataStoreObject.Options)
+		if success == false then warn("DataStore(" .. dataStoreObject.Id .. "):", value) return "DataStore", value end
+		dataStoreObject.Version = value
+	else
+		local level = dataStoreObject.Metadata.Compress.Level or 2
+		local decimals = 10 ^ (dataStoreObject.Metadata.Compress.Decimals or 3)
+		local safety = if dataStoreObject.Metadata.Compress.Safety == nil then true else dataStoreObject.Metadata.Compress.Safety
+		dataStoreObject.CompressedValue = Compress(dataStoreObject.Value, level, decimals, safety)
+		dataStoreObject.Options:SetMetadata(dataStoreObject.Metadata)
+		local success, value = pcall(dataStoreObject.DataStore.SetAsync, dataStoreObject.DataStore, dataStoreObject.Key, dataStoreObject.CompressedValue, dataStoreObject.UserIds, dataStoreObject.Options)
 		if success == false then warn("DataStore(" .. dataStoreObject.Id .. "):", value) return "DataStore", value end
 		dataStoreObject.Version = value
 	end
@@ -341,12 +385,101 @@ end
 
 Reconcile = function(target, template)	
 	for index, value in template do
+		if type(index) == "number" then continue end
 		if target[index] == nil then
 			target[index] = Clone(value)
 		elseif type(target[index]) == "table" and type(value) == "table" then
 			Reconcile(target[index], value)
 		end
 	end
+end
+
+Compress = function(value, level, decimals, safety)
+	local data = {}
+	if type(value) == "boolean" then
+		table.insert(data, if value == false then "-" else "+")
+	elseif type(value) == "number" then
+		if value % 1 == 0 then
+			table.insert(data, if value < 0 then "<" .. Encode(-value) else ">" .. Encode(value))
+		else
+			table.insert(data, if value < 0 then "(" .. Encode(math.round(-value * decimals)) else ")" .. Encode(math.round(value * decimals)))
+		end
+	elseif type(value) == "string" then
+		if safety == true then value = value:gsub("", " ") end
+		table.insert(data, "#" .. value .. "")
+	elseif type(value) == "table" then
+		if #value > 0 and level == 2 then
+			table.insert(data, "|")
+			for i = 1, #value do table.insert(data, Compress(value[i], level, decimals, safety)) end
+			table.insert(data, "")
+		else
+			table.insert(data, "*")
+			for key, tableValue in value do table.insert(data, Compress(key, level, decimals, safety)) table.insert(data, Compress(tableValue, level, decimals, safety)) end
+			table.insert(data, "")
+		end
+	end
+	return table.concat(data)
+end
+
+Decompress = function(value, decimals, index)	
+	local i1, i2, dataType, data = value:find("([-+<>()#|*])", index or 1)
+	if dataType == "-" then
+		return false, i2
+	elseif dataType == "+" then
+		return true, i2
+	elseif dataType == "<" then
+		i1, i2, data = value:find("([^-+<>()#|*]*)", i2 + 1)
+		return -Decode(data), i2
+	elseif dataType == ">" then
+		i1, i2, data = value:find("([^-+<>()#|*]*)", i2 + 1)
+		return Decode(data), i2
+	elseif dataType == "(" then
+		i1, i2, data = value:find("([^-+<>()#|*]*)", i2 + 1)
+		return -Decode(data) / decimals, i2
+	elseif dataType == ")" then
+		i1, i2, data = value:find("([^-+<>()#|*]*)", i2 + 1)
+		return Decode(data) / decimals, i2
+	elseif dataType == "#" then
+		i1, i2, data = value:find("(.-)", i2 + 1)
+		return data, i2
+	elseif dataType == "|" then
+		local array = {}
+		while true do
+			data, i2 = Decompress(value, decimals, i2 + 1)
+			if data == nil then break end
+			table.insert(array, data)
+		end
+		return array, i2
+	elseif dataType == "*" then
+		local dictionary, key = {}, nil
+		while true do
+			key, i2 = Decompress(value, decimals, i2 + 1)
+			if key == nil then break end
+			data, i2 = Decompress(value, decimals, i2 + 1)
+			dictionary[key] = data
+		end
+		return dictionary, i2
+	end
+	return nil, i2
+end
+
+Encode = function(value)
+	if value == 0 then return "0" end
+	local data = {}
+	while value > 0 do
+		table.insert(data, characters[value % base])
+		value = math.floor(value / base)
+	end
+	return table.concat(data)
+end
+
+Decode = function(value)
+	local number, power, data = 0, 1, {string.byte(value, 1, #value)}	
+	for i, code in data do
+		number += bytes[code] * power
+		power *= base
+	end
+	return number
 end
 
 
