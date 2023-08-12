d4a2eac80a0f2fce7332bb4f2563b76c27fbffbe
NameTakenBonk <83465599+NameTakenBonk@users.noreply.github.com>
2023-08-12 15:02:38 +0100

    Built and published by Moonwave

--- assets/js/1bb1f55b.77b47326.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[472],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/NameTakenBonk/SuphisDataStoreModule/blob/master","baseUrl":"/SuphisDataStoreModule/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/1bb1f55b.fbf18234.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[472],{63891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/NameTakenBonk/SuphisDataStoreModule/blob/master","baseUrl":"/SuphisDataStoreModule/","classOrder":["Constructor","DataStore","Proxy","Signal","TaskManager"],"apiCategories":[]}')}}]);
